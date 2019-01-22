07b9675b1c4885b722d06afe3a036d4f7ce3198c
aclaudio123 <aclaudio123@gmail.com>
2019-01-22 14:50:24 -0500

    Delete ex1query2.sql

--- Crs4_python_databases/wk1_SQL/ex1query2.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM Ages;
-INSERT INTO Ages (name, age) VALUES ('Amelka', 40);
-INSERT INTO Ages (name, age) VALUES ('Jerry', 16);
-INSERT INTO Ages (name, age) VALUES ('Bruce', 30);
-INSERT INTO Ages (name, age) VALUES ('Jasim', 32);
