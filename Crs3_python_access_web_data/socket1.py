f2d1e38c4e9ca59883fe4492668e32cce8abf35b
aclaudio123 <aclaudio123@gmail.com>
2019-01-22 12:21:38 -0500

    Retrieve web page over a Network Socket

--- Crs3_python_access_web_data/socket1.py
+++ Crs3_python_access_web_data/socket1.py
@@ -2,13 +2,12 @@
 # Title:   Retrieving web pages over Network Sockets
 # Author: Claudio Asangong
 #
-# Simple Python program to retrieve a web page over a socket and display
-# the headers and content from the web server. An alternative to opening
-# the URL in a web browser with a developer console and manually examine
-# the headers that are returned.
+# Simple Python program to retrieve a web page over a Network socket and
+# display the headers and content from the web server. An alternative to
+# opening the URL in a web browser with a developer console and manually
+# examine the headers that are returned.
 #
 # Concepts: Network Sockets, HTTP
-# commit msg: Retrieve web page over a socket
 
 import socket
 
@@ -16,7 +15,7 @@ import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 
-# send request
+# send request as UTF-8 bytes
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
@@ -26,6 +25,7 @@ while True:
     # when end of file break the loop
     if len(data) < 1:
         break
+    # decode UTF-8 bytes to python unicode strings    
     print(data.decode(), end='')
 
 mysock.close()
